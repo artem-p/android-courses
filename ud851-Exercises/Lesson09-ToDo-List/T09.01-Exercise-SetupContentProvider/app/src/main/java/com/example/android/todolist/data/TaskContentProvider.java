5c5126758b5e672265f93a19512d04daec5b44f6
artem-p <artempugachev1@gmail.com>
2017-03-28 11:59:18 +0300

    t0901

--- ud851-Exercises/Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ ud851-Exercises/Lesson09-ToDo-List/T09.01-Exercise-SetupContentProvider/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -18,25 +18,26 @@ package com.example.android.todolist.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
-// TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
+import com.example.android.todolist.MainActivity;
+
 public class TaskContentProvider extends ContentProvider {
 
-
+    private TaskDbHelper mDbHelper;
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
     In this case, you’re working with a SQLite database, so you’ll need to
     initialize a DbHelper to gain access to it.
      */
+
     @Override
     public boolean onCreate() {
-        // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
-        // [Hint] Declare the DbHelper as a global variable
-
-        return false;
+        mDbHelper = new TaskDbHelper(getContext());
+        return true;
     }
 
 

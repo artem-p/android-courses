8f2cd5eb6a7adff0a3ece1a14c3094701d43a2fc
artem-p <artempugachev1@gmail.com>
2017-04-25 11:19:23 +0300

    s1003

--- ud851-Sunshine/S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ ud851-Sunshine/S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -17,11 +17,47 @@ package com.example.android.sunshine.sync;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
+    private static boolean sInitialized;
 
+    synchronized public static void initialize(@NonNull final Context context) {
+        if (!sInitialized) {
+            sInitialized = true;
+
+            new AsyncTask<Void, Void, Void>() {
+
+                @Override
+                protected Void doInBackground(Void... params) {
+                    Uri forecastQueryUri = WeatherContract.WeatherEntry.CONTENT_URI;
+                    String[] projectionColumns = {WeatherContract.WeatherEntry._ID};
+                    String selectionStatement = WeatherContract.WeatherEntry.getSqlSelectForTodayOnwards();
+
+                    Cursor cursor = context.getContentResolver().query(
+                            forecastQueryUri,
+                            projectionColumns,
+                            selectionStatement,
+                            null,
+                            null
+                    );
+
+                    if (cursor == null || cursor.getCount() == 0) {
+                        startImmediateSync(context);
+                    }
+
+                    cursor.close();
+                    return null;
+                }
+            }.execute();
+        }
+    }
 //  TODO (1) Declare a private static boolean field called sInitialized
 
     //  TODO (2) Create a synchronized public static void method called initialize
